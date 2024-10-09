2c3ad4aa8bb0c72627e70be8a34cf9ac587ff47f
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-10-09 09:38:58 -0300

    third

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from 'react'
+import style from '../styles/Home.module.css'
 import data from '../../data.json'
 
 export const Card = () => {
     const [datos, setDatos] = useState(data)
     useEffect(()=>{
-     },[])
-  return (
-    <div>
+    },[])
+    return (
+        <div className={style.todo}>
         {
             datos.map((item, index) => (
-                <div key={index}>
-                     <h1> {item.name} </h1>
+                <div key={index} className={style.card}>
+                <img src= {item.image.desktop} />
+                    <p> {item.category} </p>
+                    <h3> {item.name} </h3>
+                    <p> ${
+                       item.price.toLocaleString('es-Es',{
+                        minimumFractionDigits:2,
+                        maximumFractionDigits:2
+                       })
+                    } </p>
                 </div>
             ))
         }
