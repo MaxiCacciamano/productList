9458a0dfb2738c7266ecc33e339bf314dfffb74b
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-10-15 11:02:16 -0300

    l

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,9 +7,15 @@ import { CartContext } from '../CartContext';
 export const Cart = ({}) => {
   const {carrito,setCarrito, agregarAlCarrito ,eliminarDelCarrito, exist} = useContext(CartContext)
 
+  const precioTotal = carrito.reduce((total, producto)=>(total + (producto.price * producto.quantity)), 0)
+  const decimales = <h3> ${precioTotal.toLocaleString('es-Es', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  })} </h3>
+
   return (
     <div className={style.cart}>
-      <h2>Your cart</h2>
+      <h2 style={{color:'hsl(14, 86%, 42%)'}}>Your cart</h2>
       {
         carrito.length === 0 ?(
           <p>Carrito vacio</p>
@@ -19,12 +25,12 @@ export const Cart = ({}) => {
           carrito.map((producto, index)=>(
             <li key={producto.id} className={{}}>
               <div className={style.product}>
-                  <p>{producto.name}</p>
+                  <p style={{fontWeight:'600'}}>{producto.name}</p>
                   <div style={{display:'flex'}}>
-                  <p style={{color:'hsl(14, 86%, 42%)', fontWeight:'bold', marginRight:'10px'}}>{producto.quantity}x</p> <p style={{color:'hsl(14, 25%, 72%)'}}>{"@"}{producto.price}&nbsp;&nbsp;&nbsp;{producto.price * producto.quantity}</p>  
+                  <p style={{color:'hsl(14, 86%, 42%)', fontWeight:'bold', marginRight:'10px'}}>{producto.quantity}x</p> &nbsp;&nbsp;<p style={{color:'hsl(14, 25%, 72%)'}}>{"@"}${producto.price}&nbsp;&nbsp;&nbsp;${ producto.price * producto.quantity}</p>  
                   </div>
              </div>
-              <div>
+              <div style={{alignContent:'center'}}>
                   <img onClick={(()=>eliminarDelCarrito(producto))} src={remove}/>
               </div>
             </li>
@@ -35,14 +41,15 @@ export const Cart = ({}) => {
         
       }
       <div style={{display:'flex', justifyContent:'space-between'}}>
-          <h4> Total:</h4>
+          <p style={{alignContent:'center', alignItems:'center'}}> Total</p> 
           {
             carrito.length > 0 &&( 
-              <h3>${carrito.reduce((total, producto)=>total + (producto.price * producto.quantity), 0)}</h3>
+              <h3>{decimales}</h3>
+              
             )
           }
       </div>
-      <button>Confirm order</button>
+      <button className={style.confirm}>Confirm order</button>
     </div>
   )
 }
