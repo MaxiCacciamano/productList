8476994e454fd9cb4694d732ac2b8f65a7b7a3ed
MaxiCacciamano <maximilianocacciamano@gmail.com>
2024-10-22 12:36:52 -0300

    w

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import {composeWithDevTools} from '@redux-devtools/extension';
 import someFeatureReducer from '../features/someFeature/someFeatureSlice';
 
 const store = configureStore({
